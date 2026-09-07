032cec58786a9ef8a620fd4bc70b661703b02a90
SmartAppForge <procontacter@gmail.com>
2026-09-07 03:17:28 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1424,7 +1424,7 @@ function updateProgress() {
 
 
         DOM.questionCounter.textContent =
-            `Questions ${firstQuestionNumber}â€“${lastQuestionNumber} / ${totalQuestions}`;
+            `Questions ${firstQuestionNumber}→${lastQuestionNumber} / ${totalQuestions}`;
 
     }
 
